b566a89e8ffc6c88ea1dff3f1ea7035c5b9dd7e9
allyscoding <allyc@umich.edu>
2022-12-06 06:26:27 -0500

    commit5

---
+++ js/jsClay.js
@@ -0,0 +1,61 @@
+
+
+function newtulip()
+{
+    document.getElementById("image").src = "images/tulip2.jpeg";
+}
+
+function oldtulip()
+{
+    document.getElementById("image").src = "images/tulip.jpeg";
+}
+
+function newkeychain()
+{
+    document.getElementById("image2").src = "images/clay_keychain.jpeg";
+}
+
+function oldkeychain()
+{
+    document.getElementById("image2").src = "images/tulip4.jpeg";
+}
+
+function newrose()
+{
+    document.getElementById("image3").src = "images/rose2.jpg";
+}
+
+function oldrose()
+{
+    document.getElementById("image3").src = "images/rose.jpg";
+}
+
+function newsunflower()
+{
+    document.getElementById("image4").src = "images/sunflower2.jpeg";
+}
+
+function oldsunflower()
+{
+    document.getElementById("image4").src = "images/sunflower1.jpeg";
+}
+
+function newoval()
+{
+    document.getElementById("image5").src = "images/oval2.jpeg";
+}
+
+function oldoval()
+{
+    document.getElementById("image5").src = "images/oval.jpg";
+}
+
+function newjade()
+{
+    document.getElementById("image6").src = "images/jade2.jpeg";
+}
+
+function oldjade()
+{
+    document.getElementById("image6").src = "images/jade.jpeg";
+}
